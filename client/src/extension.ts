7bd8f0c2fdceee435be07519ede1cce27231dc4e
yasutani <yasutani_naoto@uniquevision.co.jp>
2021-11-02 20:06:18 +0900

    rename project name.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -47,8 +47,8 @@ export function activate(context: ExtensionContext) {
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    'languageServerExample',
-    'Language Server Example',
+    'plPgSqlLanguageServer',
+    'PL/pgSQL Language Server',
     serverOptions,
     clientOptions
   );
--- server/src/server.ts
+++ server/src/server.ts
@@ -100,7 +100,7 @@ connection.onDidChangeConfiguration(change => {
     documentSettings.clear();
   } else {
     globalSettings = <ExampleSettings>(
-      (change.settings.languageServerExample || defaultSettings)
+      (change.settings.plPgSqlLanguageServer || defaultSettings)
     );
   }
 
@@ -116,7 +116,7 @@ function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
   if (!result) {
     result = connection.workspace.getConfiguration({
       scopeUri: resource,
-      section: 'languageServerExample'
+      section: 'plPgSqlLanguageServer'
     });
     documentSettings.set(resource, result);
   }
