3264055ff4817867dc86fcc8ce4884b0d6d5ffae
Yasutanium <yasutanium0740@outlook.com>
2022-01-31 01:17:40 +0900

    update CreateFunctionStmt.

--- server/src/postgres/statement.ts
+++ server/src/postgres/statement.ts
@@ -33,6 +33,8 @@ export interface CompositeTypeStmtTypevar {
 }
 
 export interface CreateFunctionStmt {
+  is_procedure: boolean
+  replace: boolean
   funcname: CreateFunctionStmtFuncName[]
   returnType: CreateFunctionStmtReturnType
   options: CreateFunctionStmtOption[]
