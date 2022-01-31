cdfb734c6fb67b8d0b597de22d7944a2526c9da4
Yasutanium <yasutanium0740@outlook.com>
2022-01-31 21:58:01 +0900

    update package.

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -93,7 +93,7 @@ export default {
     // notifyMode: "failure-change",
 
     // A preset that is used as a base for Jest's configuration
-    preset: "jest-puppeteer",
+    // preset: "jest-puppeteer",
 
     // Run tests from one or more projects
     // projects: undefined,
