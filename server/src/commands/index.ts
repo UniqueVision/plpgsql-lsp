06b1b052c97e313cc19e1392be845a0e5ecf65bf
Yasutanium <yassun4dev@outlook.com>
2022-05-31 01:59:13 +0900

    Add: newline.

--- server/src/commands/index.ts
+++ server/src/commands/index.ts
@@ -1,7 +1,9 @@
 import { FILE_QUERY_COMMAND } from "./executeFileQuery"
 
 export const COMMANDS = [FILE_QUERY_COMMAND] as const
+
 export const COMMAND_NAMES = COMMANDS.map(command => command.name)
+
 export const COMMAND_TITLE_MAP = Object.fromEntries(
   COMMANDS.map(command => [command.name, command.title]),
 )
